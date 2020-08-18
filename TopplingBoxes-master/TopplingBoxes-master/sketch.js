31335497af58828174c54ea472e777eb0d65e1b5
pg775 <67814029+pg775@users.noreply.github.com>
2020-08-18 11:04:44 -0700

    Add files via upload

---
+++ TopplingBoxes-master/TopplingBoxes-master/sketch.js
@@ -0,0 +1,53 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1;
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    ground = new Ground(600,height,1200,20)
+    pig = new Pig(810,350)
+    log = new Log(810,260,300,PI/2)
+
+    box3 = new Box(700,240,70,70);
+    box4 = new Box(920,240,70,70);
+    pig1 = new Pig(810,150)
+    log1= new Log(810,100,300,PI/2)
+
+   
+    box5 = new Box(810,90,70,70)
+    log2= new Log(760,60,150,PI/7)
+    log3 = new Log(870,60,150,-PI/7)
+
+    bird = new Bird(200,200)
+
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    console.log(box2.body.position.x);
+    console.log(box2.body.position.y);
+    console.log(box2.body.angle);
+    box1.display();
+    box2.display();
+    ground.display();
+    pig.display();
+    log.display();
+    box3.display();
+    box4.display();
+    pig1.display();
+    log1.display(); 
+    box5.display();
+    log2.display();
+    log3.display();
+    bird.display();
+
+}
